59294722ee75125d5fd6aa100f8772042a28bcc0
JacobChao <jacob.chao@wavegis.com.tw>
2019-04-16 12:08:56 +0800

    修改玩家人數變化邏輯

--- app/src/main/java/org/jc/avalonsetting/DrawActivity.kt
+++ app/src/main/java/org/jc/avalonsetting/DrawActivity.kt
@@ -2,17 +2,26 @@ package org.jc.avalonsetting
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Debug
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.app.AppCompatActivity
 import android.view.KeyEvent
 import android.view.View
+import android.view.View.VISIBLE
 import android.widget.TextView
+import com.socks.library.KLog
 import kotlinx.android.synthetic.main.activity_draw.*
+import org.jc.avalonsetting.R.id.draw
 
 
 class DrawActivity : AppCompatActivity(), View.OnClickListener {
 
     private var currentPlayer = 0
     private val players = ArrayList<TextView>()
+    private val playerObjs by lazy {
+        arrayOf(player1, player2, player3, player4, player5,
+                player6, player7, player8, player9, player10)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +31,27 @@ class DrawActivity : AppCompatActivity(), View.OnClickListener {
             GAME_SET_10 -> PLAYER_10
             else -> PLAYER_10
         }
-        players.add(player1)
-        players.add(player2)
-        players.add(player3)
-        players.add(player4)
-        players.add(player5)
-        players.add(player6)
-        players.add(player7)
-        players.add(player8)
-        players.add(player9)
-        players.add(player10)
+        playerObjs.forEach {
+            players.add(it)
+        }
         CHARACTERS.addAll(resources.getStringArray(when (Players) {
             GAME_SET_8 -> R.array.character_8
             GAME_SET_10 -> R.array.character_all
             else -> R.array.character_all
         }))
+        //填入玩家數量
+        repeat(CHARACTERS.size) {
+            players[it].visibility = VISIBLE
+            players[it].text = when (Players) {
+                GAME_SET_8 -> if (it < 7) (it + 1).toString() else "0"
+                GAME_SET_10 -> if (it < 9) (it + 1).toString() else "0"
+                else -> "0"
+            }
+        }
         //亂數排列角色
         CHARACTERS.shuffle()
-        CHARACTERS.forEach {
-            println(it)
+        if (Debug.isDebuggerConnected()) {
+            KLog.d(CHARACTERS)
         }
     }
 
