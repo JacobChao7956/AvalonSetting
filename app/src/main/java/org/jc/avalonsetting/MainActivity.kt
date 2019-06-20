d2b56225da2a66491ac2b89d110800f705adf8c5
JacobChao <jacob.chao@wavegis.com.tw>
2019-06-20 17:32:29 +0800

    修正app閃退或手動關閉時，資料庫不會被清除的問題

--- app/src/main/java/org/jc/avalonsetting/MainActivity.kt
+++ app/src/main/java/org/jc/avalonsetting/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.get
 import com.socks.library.KLog
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jc.avalonsetting.data.viewmodel.PlayerViewModel
@@ -13,10 +14,18 @@ import org.jc.avalonsetting.references.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var playerViewModel: PlayerViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = getString(R.string.title_main)
+        playerViewModel = ViewModelProviders.of(this).get(PlayerViewModel::class.java)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        playerViewModel.deleteAll()
     }
 
     override fun onClick(view: View?) {
